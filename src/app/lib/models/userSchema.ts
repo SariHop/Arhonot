e0c65ab63abd2927a96f2b880b0065faa8b0eb3f
TamarBarzel <151558351+TamarBarzel@users.noreply.github.com>
2024-11-25 21:43:12 +0200

    basic crud for user & small fixes

--- src/app/lib/models/userSchema.ts
+++ src/app/lib/models/userSchema.ts
@@ -2,7 +2,7 @@
 import IUser from "@/app/types/IUser";
 import mongoose, { Model, Schema, Types } from "mongoose";
 
-const AlertSchema : Schema<IUser> = new Schema({
+const UserSchema : Schema<IUser> = new Schema({
     children: {type: [Types.ObjectId], required:true},
     passsword: { type: String, required: true,minlength: 8, maxlength: 12, match: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,12}$/},
     email: { type: String, required: true, match: /\S+@\S+\.\S+/},
@@ -14,16 +14,15 @@ const AlertSchema : Schema<IUser> = new Schema({
     }, "Date of birth cannot be in the future"] },
     city: { type: String, required: true, validate: {
         validator: async function(value: string) {
-            const cities: string[] = [];
+            const cities: string[] = ['ירושלים'];
             return cities.includes(value);
         },
         message: "City must be one of the valid cities in Israel",
     }
 },
     sensitive: { type: String, required: true, enum: ["cold", "heat", "none"]},
-    parents: {type: [Types.ObjectId], required:true, max: 2},
     userDays: {type: [Types.ObjectId], required:true},
 })
 
-const User:Model<IUser> =mongoose.models.User || mongoose.model<IUser>('User',AlertSchema)
+const User:Model<IUser> =mongoose.models.User || mongoose.model<IUser>('User',UserSchema)
 export default User;
