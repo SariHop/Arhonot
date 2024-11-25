a92928fe69f93e6d360308ec4af57bfce1a707cf
RivkyTR <135111313+RivkyTR@users.noreply.github.com>
2024-11-25 09:49:19 +0200

    adding schma for alart

---
+++ src/app/types/IAlert.ts
@@ -0,0 +1,23 @@
+import { Document, ObjectId } from "mongoose";
+import {z} from 'zod'
+
+export default interface IAlert extends Document{
+    userId: ObjectId;
+    desc: string;
+    title: string;
+    readen: boolean;
+}
+
+
+export const alertSchemaZod = z.object({
+    desc: z.string().nullable(),
+    title: z.string().min(10,"Title is required"),
+    readen: z.boolean(), 
+});
+
+export type IAlertType = z.infer<typeof alertSchemaZod> 
+
+export type IAlertTypeWithId = z.infer<typeof alertSchemaZod> & {
+    _id: string;
+};
+
