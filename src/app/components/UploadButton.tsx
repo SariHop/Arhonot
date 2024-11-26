be089cd6d17d6a3d69435ac0cdaef2a78c031683
Sari H <154007325+SariHop@users.noreply.github.com>
2024-11-26 11:54:48 +0200

    show img

--- src/app/components/UploadButton.tsx
+++ src/app/components/UploadButton.tsx
@@ -1,18 +1,22 @@
 "use client"
-
-import React from 'react'
-import { CldUploadWidget } from 'next-cloudinary';
+import React, { useState } from 'react'
+import { CldUploadWidget, CldImage } from 'next-cloudinary';
 
 const UploadButton = () => {
-  
+  const [publicID, setPublicID] = useState("");
 
   return (
-
     <div className='w-32 m-10'>
       <CldUploadWidget
         signatureEndpoint="/api/sign-cloudinary-params"
-        uploadPreset = {process.env.NEXT_PUBLIC_CLOUDINARY_PRESET}
-        options={{ sources: ['local', 'camera',  'url', 'google_drive'] }}
+        uploadPreset={process.env.NEXT_PUBLIC_CLOUDINARY_PRESET}
+        options={{ sources: ['local', 'camera', 'url', 'google_drive'] }}
+        
+        onSuccess={(result) => {
+          if (result.info && typeof result.info === 'object' && 'public_id' in result.info) {
+            setPublicID(result.info.public_id as string);
+          }
+        }}
       >
         {({ open }) => {
           return (
@@ -22,9 +26,20 @@ const UploadButton = () => {
           );
         }}
       </CldUploadWidget>
+      
+      {/* send this public id in props to save in DB */}
+      {publicID && (
+        <CldImage
+          width="300"
+          height="300"
+          src={publicID}
+          sizes="100vw"
+          alt="Uploaded image"
+          className="mt-4 object-cover"
+        />
+      )}
     </div>
-
-  )
+  );
 }
 
-export default UploadButton
+export default UploadButton;
