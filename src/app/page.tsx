8b4fb532c94e04e3459eae3b9dcbf6b10cca9eb1
Sari H <154007325+SariHop@users.noreply.github.com>
2024-12-01 15:20:42 +0200

    vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
-"use client"
-import React, { useState } from 'react';
+import React from 'react';
 import { ToastContainer} from "react-toastify";
-import UploadImage from './components/imagesUploud/UploudButton';
 
 const ExamplePage: React.FC = () => {
-  const [x, setX]=useState("")
 
   return (
     <div className="p-4">
       {/*react-toastify הצגת שגיאות עם שימוש בספריה */}
       <ToastContainer />
-      <UploadImage setCloudinary={setX}/>
 
 
     </div>
