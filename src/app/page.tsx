01b9fa220d030ebef065b58c6e1bf9dabeab4716
Sari H <154007325+SariHop@users.noreply.github.com>
2024-12-02 09:16:41 +0200

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
-import { ToastContainer} from "react-toastify";
+"use state"
+import React, { useState } from 'react';
+import { ToastContainer } from "react-toastify";
+import Image from 'next/image';
+import UploadImage from './components/imagesUploud/UploudButton';
 
 const ExamplePage: React.FC = () => {
+  const [x, setX] = useState('')
 
   return (
     <div className="p-4">
       {/*react-toastify הצגת שגיאות עם שימוש בספריה */}
       <ToastContainer />
 
+      <UploadImage setCloudinary={setX}></UploadImage>
 
+      {x && <Image
+        width={300}
+        height={300}
+        src={x}
+        alt='smthing'
+      />
+      }
     </div>
   );
 };
