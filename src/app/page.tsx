c8182409802cf0676323cb3d32211d02657174c9
Sari H <154007325+SariHop@users.noreply.github.com>
2024-12-04 09:04:09 +0200

    global css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ const ExamplePage: React.FC = () => {
   const router = useRouter()
 
   return (
-    <div className=" bg-gradient-to-br from-coral to-magenta w-screen h-screen flex justify-center items-center p-6 sm:py-10 sm:px-0">
+    <div className=" from-coral-to-magenta full-screen flex justify-center w-screen h-screen items-center p-6 sm:py-10 sm:px-0">
       <div className="bg-white h-full w-full flex justify-center items-center shadow-lg">
         <div className="flex flex-col items-center justify-center pb-20 animate-fade-up">
           {/* hero */}
@@ -23,7 +23,7 @@ const ExamplePage: React.FC = () => {
           <p className="text-sm text-gray-700 mb-7">צלמו, ערכו וקבלו המלצות</p>
           <button
           onClick={()=>{router.push('/pages/signin')}}
-           className=" relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-lg font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-coral to-magenta group-hover:from-coral group-hover:to-magenta hover:text-white">
+           className=" relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-lg font-medium text-gray-900 rounded-lg group from-coral-to-magenta group-hover:from-coral group-hover:to-magenta hover:text-white">
             <span className="relative flex items-center px-5 py-1 transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-opacity-0">
               בואו נתחיל!
               <ArrowLeftOutlined className="pr-2" />
