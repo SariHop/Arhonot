df9fe8b687c554a8d3fa7941656f18a3db08b07c
TamarBarzel <151558351+TamarBarzel@users.noreply.github.com>
2024-12-07 22:02:05 +0200

    header with true location

--- src/app/hooks/weatherQueryHook.ts
+++ src/app/hooks/weatherQueryHook.ts
@@ -1,12 +1,21 @@
 import { useQuery } from '@tanstack/react-query';
 import {fetchWeatherData} from '@/app/services/weatherService'
 import { WeatherData} from '@/app/types/IWeather'
+import { toast } from 'react-toastify';
+
 
 export const useWeatherQuery = () => {
   return useQuery<WeatherData, Error>({
     queryKey: ['weatherData'],
-    queryFn: fetchWeatherData,
-    staleTime: 24 * 60 * 60 * 1000, // נתונים תקפים ל-24 שעות
-    refetchInterval: 24 * 60 * 60 * 1000, // רענון אחת ל-24 שעות
-  });
-};
+    queryFn: async () => {
+        try {
+          return await fetchWeatherData();
+        } catch (error) {
+          toast.error('לא הצלחנו למשוך את נתוני מזג האוויר. נסה שוב מאוחר יותר.');
+          throw error;  
+        }
+      },
+      staleTime: 24 * 60 * 60 * 1000, // נתונים תקפים ל-24 שעות
+      refetchInterval: 24 * 60 * 60 * 1000, // רענון אחת ל-24 שעות
+    });
+  };
