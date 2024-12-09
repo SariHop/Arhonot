368a161f548886591a85e9b4a36f04a24eb192de
EstyBlotnik <118099586+EstyBlotnik@users.noreply.github.com>
2024-12-09 10:42:43 +0200

    pagination

--- src/app/components/galery/garments/Garment.tsx
+++ src/app/components/galery/garments/Garment.tsx
@@ -1,7 +1,7 @@
 "use client";
-import React from 'react'
+import React from "react";
 import Image from "next/image";
-import { GarmentProps } from '@/app/types/IGarment';
+import { GarmentProps } from "@/app/types/IGarment";
 
 const Garment = ({ garment, closeModal }: GarmentProps) => {
     const handleCloseClick = (e: React.MouseEvent) => {
@@ -15,35 +15,66 @@ const Garment = ({ garment, closeModal }: GarmentProps) => {
             onClick={(e) => e.stopPropagation()} // מונע סגירה בלחיצה על המודאל עצמו
         >
             <div
-                className="bg-white p-6 rounded-lg shadow-lg w-full max-w-lg relative"
+                className="bg-white p-4 rounded-lg shadow-xl w-full max-w-md relative text-right"
                 onClick={(e) => e.stopPropagation()} // מונע סגירה בלחיצה על התוכן
+                dir="rtl"
             >
                 <button
                     onClick={handleCloseClick}
-                    className="absolute top-2 right-2 text-xl text-red-500"
+                    className="absolute top-2 left-2 text-2xl text-red-500 hover:text-red-700 transition"
                 >
-                    X
+                    ✖
                 </button>
-                <h2 className="text-2xl font-semibold text-center">{garment.desc}</h2>
-                <Image
-                    src={garment.img}
-                    alt={garment.desc}
-                    width={200}
-                    height={300}
-                    className="object-cover rounded-lg mt-4 text-center"
-                />
-                <div className="mt-4">
-                    <p><strong>Season:</strong> {garment.season}</p>
-                    <p><strong>Range:</strong> {garment.range}</p>
-                    <p><strong>Category:</strong> {garment.category}</p>
-                    <p><strong>Color:</strong> <div style={{ backgroundColor: garment.color }}>{garment.color}</div></p>
-                    <p><strong>Price:</strong> ${garment.price}</p>
-                    <p><strong>Link:</strong> <a href={garment.link} target="_blank" className="text-blue-500">Visit Link</a></p>
-                    <p><strong>Tags:</strong> {garment.tags.join(", ")}</p>
+                <h2 className="text-2xl font-semibold text-center mb-4 text-gray-800">
+                    {garment.desc}
+                </h2>
+                <div className="flex flex-col items-center">
+                    <Image
+                        src={garment.img}
+                        alt={garment.desc}
+                        width={180}
+                        height={270}
+                        className="object-cover rounded-lg mt-4"
+                    />
+                    <div className="mt-6 w-full space-y-2">
+                        <p className="text-gray-700">
+                            <strong>עונה:</strong> {garment.season}
+                        </p>
+                        <p className="text-gray-700">
+                            <strong>טווח:</strong> {garment.range}
+                        </p>
+                        <p className="text-gray-700">
+                            <strong>קטגוריה:</strong> {garment.category}
+                        </p>
+                        <p className="text-gray-700 flex items-center">
+                            <strong>צבע:  </strong>
+                            <div
+                                style={{ backgroundColor: garment.color }}
+                                className="w-6 h-6 rounded-full border mx-2"
+                            ></div>
+                        </p>
+                        <p className="text-gray-700">
+                            <strong>מחיר:</strong> ${garment.price}
+                        </p>
+                        <p className="text-gray-700">
+                            <strong>קישור:</strong>{" "}
+                            <a
+                                href={garment.link}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="text-blue-500 hover:underline"
+                            >
+                                מעבר לקישור
+                            </a>
+                        </p>
+                        <p className="text-gray-700">
+                            <strong>תגיות:</strong> {garment.tags.join(", ")}
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Garment
+export default Garment;
--- src/app/components/galery/garments/GarmetsGallery.tsx
+++ src/app/components/galery/garments/GarmetsGallery.tsx
@@ -1,21 +1,58 @@
 "use client";
-import React from "react";
+import { useState, useEffect } from "react";
 import Card from "./Card";
 import useGarments from '../../../store/garmentsStore';
 
+const ITEMS_PER_PAGE = 4; // מספר פריטים לעמוד
 
 const GarmentsGallery = ({ isForOutfit }: { isForOutfit: boolean }) => {
     const { sortedGarments } = useGarments();
-    // if (!sortedGarments.length) return <p>No garments found.</p>;
+    const [currentPage, setCurrentPage] = useState(1); // מעקב אחר העמוד הנוכחי
 
+    const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+    const endIndex = startIndex + ITEMS_PER_PAGE;
+    const currentItems = sortedGarments.slice(startIndex, endIndex);
+
+    const totalPages = Math.ceil(sortedGarments.length / ITEMS_PER_PAGE);
+
+    useEffect(() => {
+        setCurrentPage(1);
+    }, [sortedGarments]);
+
+    const goToPreviousPage = () => setCurrentPage((prev) => Math.max(prev - 1, 1));
+    const goToNextPage = () => setCurrentPage((prev) => Math.min(prev + 1, totalPages));
     return (
         <>
             {!sortedGarments.length && <p>No garments found.</p>}
             <div className="grid grid-cols-3 sm:grid-cols-5 md:grid-cols-5 lg:grid-cols-7 gap-4 px-4">
-                {sortedGarments.map((garment) => (
+                {currentItems.map((garment) => (
                     <Card key={String(garment._id)} garment={garment} isForOutfit={isForOutfit} />
                 ))}
             </div>
+            {sortedGarments.length > ITEMS_PER_PAGE && (
+                <div className="flex justify-center items-center mt-6 space-x-6">
+                    <button
+                        onClick={goToPreviousPage}
+                        disabled={currentPage === 1}
+                        className={`text-gray-600 underline 
+                        ${currentPage === 1 ? 'text-gray-400 no-underline cursor-not-allowed' : 'hover:text-gray-800'}`}
+                    >
+                        הקודם
+                    </button>
+                    <span className="text-gray-600 font-medium">
+                        Page <span className="font-bold">{currentPage}</span> of <span className="font-bold">{totalPages}</span>
+                    </span>
+                    <button
+                        onClick={goToNextPage}
+                        disabled={currentPage === totalPages}
+                        className={`text-gray-600 underline 
+                        ${currentPage === totalPages ? 'text-gray-400 no-underline cursor-not-allowed' : 'hover:text-gray-800'}`}
+                    >
+                        הבא
+                    </button>
+                </div>
+            )}
+
         </>
     );
 };
