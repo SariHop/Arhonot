ede3cb94c68d9936032a4f54b009936a4a7444a2
RivkyTR <135111313+RivkyTR@users.noreply.github.com>
2024-12-11 09:39:12 +0200

    vercel

--- src/app/pages/layout.tsx
+++ src/app/pages/layout.tsx
@@ -3,7 +3,7 @@ import NavBar from "../components/NavBar";
 import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-// import Header from "../components/Header";
+import Header from "../components/Header";
 
 
 
