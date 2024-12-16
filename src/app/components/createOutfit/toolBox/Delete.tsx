9fdc787e06f744afde2fe52cd17cd0893d0f04b0
Sari H <154007325+SariHop@users.noreply.github.com>
2024-12-16 14:05:05 +0200

    vercel

--- src/app/components/createOutfit/toolBox/Delete.tsx
+++ src/app/components/createOutfit/toolBox/Delete.tsx
@@ -1,65 +1,75 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import useCanvasStore from "@/app/store/canvasStore";
-import * as fabric from 'fabric';
-import { IconButton } from '@mui/material';
-import { FaTrash } from 'react-icons/fa';
+import React from 'react'
+
+const Delete = () => {
+  return (
+    <div>Delete</div>
+  )
+}
+
+export default Delete
+
+// "use client";
+// import React, { useEffect, useState } from "react";
+// import useCanvasStore from "@/app/store/canvasStore";
+// import * as fabric from 'fabric';
+// import { IconButton } from '@mui/material';
+// import { FaTrash } from 'react-icons/fa';
 
 
-const DeleteButton: React.FC = () => {
-    const [selectedObject, setSelectedObject] = useState<fabric.Object | null>(null);
-    const { canvas } = useCanvasStore();
+// const DeleteButton: React.FC = () => {
+//     const [selectedObject, setSelectedObject] = useState<fabric.Object | null>(null);
+//     const { canvas } = useCanvasStore();
 
-    useEffect(() => {
-        if (!canvas) return;
+//     useEffect(() => {
+//         if (!canvas) return;
 
-        const handleSelectionCreated = (event: fabric.IEvent) => {
-            if (event.selected && event.selected.length > 0) {
-                handleObjectSelection(event.selected[0]);
-            }
-        };
+//         const handleSelectionCreated = (event: fabric.IEvent) => {
+//             if (event.selected && event.selected.length > 0) {
+//                 handleObjectSelection(event.selected[0]);
+//             }
+//         };
 
-        const handleSelectionUpdated = (event: fabric.IEvent) => {
-            if (event.selected && event.selected.length > 0) {
-                handleObjectSelection(event.selected[0]);
-            }
-        };
+//         const handleSelectionUpdated = (event: fabric.IEvent) => {
+//             if (event.selected && event.selected.length > 0) {
+//                 handleObjectSelection(event.selected[0]);
+//             }
+//         };
 
-        const handleSelectionCleared = () => clearSelection();
+//         const handleSelectionCleared = () => clearSelection();
 
-        canvas.on("selection:created", handleSelectionCreated);
-        canvas.on("selection:updated", handleSelectionUpdated);
-        canvas.on("selection:cleared", handleSelectionCleared);
+//         canvas.on("selection:created", handleSelectionCreated);
+//         canvas.on("selection:updated", handleSelectionUpdated);
+//         canvas.on("selection:cleared", handleSelectionCleared);
 
-        return () => {
-            canvas.off("selection:created", handleSelectionCreated);
-            canvas.off("selection:updated", handleSelectionUpdated);
-            canvas.off("selection:cleared", handleSelectionCleared);
-        };
-    }, [canvas]);
+//         return () => {
+//             canvas.off("selection:created", handleSelectionCreated);
+//             canvas.off("selection:updated", handleSelectionUpdated);
+//             canvas.off("selection:cleared", handleSelectionCleared);
+//         };
+//     }, [canvas]);
 
-    const handleObjectSelection = (object: fabric.Object) => {
-        setSelectedObject(object);
-    };
+//     const handleObjectSelection = (object: fabric.Object) => {
+//         setSelectedObject(object);
+//     };
 
-    const clearSelection = () => {
-        setSelectedObject(null);
-    };
+//     const clearSelection = () => {
+//         setSelectedObject(null);
+//     };
 
-    const handleDelete = () => {
-        if (canvas && selectedObject) {
-            canvas.remove(selectedObject);
-            clearSelection();
-        }
-    };
+//     const handleDelete = () => {
+//         if (canvas && selectedObject) {
+//             canvas.remove(selectedObject);
+//             clearSelection();
+//         }
+//     };
 
-    return (
-        <div>
-            <IconButton color="error" disabled={!selectedObject} onClick={handleDelete}>
-                <FaTrash />
-            </IconButton>
-        </div>
-    );
-};
+//     return (
+//         <div>
+//             <IconButton color="error" disabled={!selectedObject} onClick={handleDelete}>
+//                 <FaTrash />
+//             </IconButton>
+//         </div>
+//     );
+// };
 
-export default DeleteButton;
+// export default DeleteButton;
