fce32fef95aa668b2b7577af2c8b8fa4b7e482cb
Sari H <154007325+SariHop@users.noreply.github.com>
2024-12-18 21:35:12 +0200

    vercel, kind a less happy it was weird error |:

--- src/app/components/createOutfit/toolBox/Delete.tsx
+++ src/app/components/createOutfit/toolBox/Delete.tsx
@@ -10,22 +10,20 @@ const DeleteButton: React.FC = () => {
   const handleDelete = () => {
     debugger;
     if (canvas && selectedObject) {
-      // בדוק תחילה אם האובייקט הוא תמונה
-      if (selectedObject.type === 'image') {
-        // עשה Type Assertion כדי ליידע את TypeScript שמדובר ב-fabric.Image
-        const imageObject = selectedObject as fabric.Image;
-  
+      // בדוק אם האובייקט הוא מסוג fabric.Image
+      if (selectedObject instanceof fabric.Image) {
+        const imageObject = selectedObject;
+
         // בדוק אם יש garmentId
         const data = imageObject.garmentId;
         if (data) {
-          canvas.remove(selectedObject);
+          canvas.remove(imageObject);
           deleteGarment(data); // השתמש ב-garmentId
           setSelectedObject(null);
         }
       }
     }
   };
-  
 
   return (
     <div>
