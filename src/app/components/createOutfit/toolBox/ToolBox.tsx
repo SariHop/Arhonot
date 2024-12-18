78b83d27a720c878c0574c6040f415229569ca63
Sari H <154007325+SariHop@users.noreply.github.com>
2024-12-18 21:54:18 +0200

    tsconfig fabric.d.ts

--- src/app/components/createOutfit/toolBox/ToolBox.tsx
+++ src/app/components/createOutfit/toolBox/ToolBox.tsx
@@ -1,76 +1,66 @@
-import React from 'react'
+"use client";
+import React, { useEffect } from "react";
+// import ButtonCreateOutfit from "@/app/components/createOutfit/toolBox/create/CreateOutfit";
+// import DeleteButton from "./Delete";
+import useCanvasStore from "@/app/store/canvasStore";
+// import ClearButton from "./Clear";
 
 const ToolBox = () => {
+
+  const { canvas, setSelectedObject } = useCanvasStore();
+
+  useEffect(() => {
+    if (!canvas) return;
+    // יצירת מאזינים לבחירה של אלמנט על הקנבס ושמירת האובייקט הנבחר בסטור
+
+    const handleSelectionCreated = (event: fabric.IEvent) => {
+      if (event.selected && event.selected.length > 0) {
+        setSelectedObject(event.selected[0]);
+      }
+    };
+    const handleSelectionUpdated = (event: fabric.IEvent) => {
+      if (event.selected && event.selected.length > 0) {
+        setSelectedObject(event.selected[0]);
+      }
+    };
+    const handleSelectionCleared = () => setSelectedObject(null);
+
+    canvas.on("selection:created", handleSelectionCreated);
+    canvas.on("selection:updated", handleSelectionUpdated);
+    canvas.on("selection:cleared", handleSelectionCleared);
+
+    return () => {
+      canvas.off("selection:created", handleSelectionCreated);
+      canvas.off("selection:updated", handleSelectionUpdated);
+      canvas.off("selection:cleared", handleSelectionCleared);
+    };
+  }, [canvas, setSelectedObject]);
+
+
   return (
-    <div>ToolBox</div>
-  )
-}
+    <div
+      className="flex overflow-x-auto whitespace-nowrap p-3 gap-5 m-1 "
+      style={{ scrollbarWidth: "thin" }}
+    >
+      {/* <ButtonCreateOutfit /> */}
 
-export default ToolBox
+      <div
+        className="flex flex-col items-center flex-shrink-0"
+        style={{ minWidth: "fit-content" }}
+      >
+        {/* <DeleteButton /> */}
+        <p className="text-sm">מחיקה</p>
+      </div>
 
-// "use client";
-// import React, { useEffect } from "react";
-// // import ButtonCreateOutfit from "@/app/components/createOutfit/toolBox/create/CreateOutfit";
-// // import DeleteButton from "./Delete";
-// import useCanvasStore from "@/app/store/canvasStore";
-// // import ClearButton from "./Clear";
+      <div
+        className="flex flex-col items-center flex-shrink-0"
+        style={{ minWidth: "fit-content" }}
+      >
+        {/* <ClearButton /> */}
+        <p className="text-sm">ניקוי</p>
+      </div>
+    </div>
+  );
+};
 
-// const ToolBox = () => {
-
-//   const { canvas, setSelectedObject } = useCanvasStore();
-
-//   useEffect(() => {
-//     if (!canvas) return;
-//     // יצירת מאזינים לבחירה של אלמנט על הקנבס ושמירת האובייקט הנבחר בסטור
-
-//     const handleSelectionCreated = (event: fabric.IEvent) => {
-//       if (event.selected && event.selected.length > 0) {
-//         setSelectedObject(event.selected[0]);
-//       }
-//     };
-//     const handleSelectionUpdated = (event: fabric.IEvent) => {
-//       if (event.selected && event.selected.length > 0) {
-//         setSelectedObject(event.selected[0]);
-//       }
-//     };
-//     const handleSelectionCleared = () => setSelectedObject(null);
-
-//     canvas.on("selection:created", handleSelectionCreated);
-//     canvas.on("selection:updated", handleSelectionUpdated);
-//     canvas.on("selection:cleared", handleSelectionCleared);
-
-//     return () => {
-//       canvas.off("selection:created", handleSelectionCreated);
-//       canvas.off("selection:updated", handleSelectionUpdated);
-//       canvas.off("selection:cleared", handleSelectionCleared);
-//     };
-//   }, [canvas, setSelectedObject]);
-
-
-//   return (
-//     <div
-//       className="flex overflow-x-auto whitespace-nowrap p-3 gap-5 m-1 "
-//       style={{ scrollbarWidth: "thin" }}
-//     >
-//       {/* <ButtonCreateOutfit /> */}
-
-//       <div
-//         className="flex flex-col items-center flex-shrink-0"
-//         style={{ minWidth: "fit-content" }}
-//       >
-//         {/* <DeleteButton /> */}
-//         <p className="text-sm">מחיקה</p>
-//       </div>
-
-//       <div
-//         className="flex flex-col items-center flex-shrink-0"
-//         style={{ minWidth: "fit-content" }}
-//       >
-//         {/* <ClearButton /> */}
-//         <p className="text-sm">ניקוי</p>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ToolBox;
+export default ToolBox;
