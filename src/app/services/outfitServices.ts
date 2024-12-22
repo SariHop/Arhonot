ffb0f1e4378c42f08cf6fd2f6e4ac47eb564d9b1
Sari H <154007325+SariHop@users.noreply.github.com>
2024-12-22 12:32:49 +0200

    servis update and toolbox layers

--- src/app/services/outfitServices.ts
+++ src/app/services/outfitServices.ts
@@ -9,13 +9,28 @@ export const apiUrl = "/api/";
 export const fetchOutfits = async (userId: Types.ObjectId) => {
     try {
         const response = await axios.get(`${apiUrl}outfitRoute/gallery/${userId.toString()}`);
-        console.log("response:");
-        console.log(response.data);
+        //console.log("response:");
+        //console.log(response.data);
         return response.data
     } catch (error) {
         console.error("Failed to fetch outfits:", error);
     }
 };
+
+export async function updateOutfit(formData: IOutfitType, id:string) {
+  try {
+    const response = await axios.put(`${apiUrl}outfitRoute/${id}`, formData);
+    return response.data.data;
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      const serverError = error.response?.data?.error || "Unknown server error";
+      toast.error(`Server Error: ${serverError}`);
+    } else {
+      toast.error("An unexpected error occurred");
+    }
+    throw error;
+  }
+}
 
 export async function createOutfit(formData: IOutfitType) {
   try {
@@ -31,6 +46,22 @@ export async function createOutfit(formData: IOutfitType) {
     throw error;
   }
 }
+
+
+// export async function createOutfit(formData: IOutfitType) {
+//   try {
+//     const response = await axios.post(`${apiUrl}outfitRoute`, formData);
+//     return response.data.data;
+//   } catch (error: unknown) {
+//     if (axios.isAxiosError(error)) {
+//       const serverError = error.response?.data?.error || "Unknown server error";
+//       toast.error(`Server Error: ${serverError}`);
+//     } else {
+//       toast.error("An unexpected error occurred");
+//     }
+//     throw error;
+//   }
+// }
 
 export async function updateOutfitFavorite(outfitId: string, favorite: number) {
   try {
