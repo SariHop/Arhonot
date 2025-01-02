0a5bebfb2f595ee4fc818aff6fb82d3f1bc2b052
TamarBarzel <151558351+TamarBarzel@users.noreply.github.com>
2025-01-02 09:27:59 +0200

    change next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,53 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "res.cloudinary.com",
-      "img.icons8.com",
-      "example.com",
-      "hahacanvas.co.il",
-      "www.photo-art.co.il",
-      "encrypted-tbn0.gstatic.com",
-      "localhost",
-      "media.geeksforgeeks.org",
-    ],
+    // domains: [
+    //   // "res.cloudinary.com",
+    //   "img.icons8.com",
+    //   "example.com",
+    //   "hahacanvas.co.il",
+    //   "www.photo-art.co.il",
+    //   "encrypted-tbn0.gstatic.com",
+    //   "localhost",
+    //   "media.geeksforgeeks.org",
+    // ],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'openweathermap.org',
-        pathname: '/img/wn/**',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "img.icons8.com",
+      },
+      {
+        protocol: "https",
+        hostname: "example.com",
+      },
+      {
+        protocol: "https",
+        hostname: "hahacanvas.co.il",
+      },
+      {
+        protocol: "https",
+        hostname: "www.photo-art.co.il",
+      },
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "https",
+        hostname: "media.geeksforgeeks.org",
+      },
+      {
+        protocol: "https",
+        hostname: "openweathermap.org",
+        pathname: "/img/wn/**",
       },
     ],
   },
