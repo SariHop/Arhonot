abbc2e31146588d6646dc9f36da6710d8cc1061c
Sari H <154007325+SariHop@users.noreply.github.com>
2025-01-02 13:36:41 +0200

    עיצוב נבבר

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -30,8 +30,8 @@ const NavBar = () => {
     initialize(user._id);
 
     // קביעת הדף הנוכחי
-    const currentPath = pathname.split("/").pop(); 
-    setSelectedPage(currentPath || ""); 
+    const currentPath = pathname.split("/").pop();
+    setSelectedPage(currentPath || "");
   }, [user._id, pathname]);
 
   // useEffect(() => {
@@ -49,7 +49,7 @@ const NavBar = () => {
 
   const handleClick = (key: string) => {
     router.push(`/pages/user/${key}`);
-        // setSelectedPage(key); 
+    // setSelectedPage(key); 
   };
 
   const onClick: MenuProps["onClick"] = ({ key }) => {
@@ -83,31 +83,31 @@ const NavBar = () => {
   ];
 
   return (
-    <div className="bg-white left-0 w-full border-t-2 h-[10vh] flex justify-around lg:justify-around items-center  border-blue-600">
+    <div className="bg-white left-0 w-full h-[10vh] flex justify-around lg:justify-around items-center border-t-2 border-blue-600 lg:border-none">
+     
       {/* פרטי משתמש */}
       <div
-        className={`flex flex-col items-center justify-center  w-full h-full hover:border-2 hover:border-blue-600  text-xl gap-1 lg:flex-row md:text-3xl ${
-          selectedPage === "personal_area" ? "bg-blue-600 text-white" : ""
-        }`}
+        className={`cursor-pointer flex flex-col items-center justify-center  w-full h-full hover:border-t-2 hover:border-t-blue-600 hover:text-blue-600 text-xl gap-1 lg:flex-row md:text-3xl
+           ${selectedPage === "personal_area" ? "border-t-blue-600 border-t-2 text-blue-600" : ""}`}
         onClick={() => handleClick("personal_area")}
       >
-        <FaUser className="text-3xl hover:text-blue-600" />
-        <span className="text-sm text-center lg:ml-2 md:text-base">משתמש</span>
+        <FaUser className="text-2xl" />
+        <span className="text-xs text-center lg:ml-2 md:text-base">משתמש</span>
       </div>
 
       {/* גלריה */}
       <div
-        className={`flex flex-col items-center justify-center  w-full h-full hover:border-2 hover:border-blue-600 text-xl gap-1 lg:flex-row md:text-3xl ${
-          selectedPage === "gallery" ? "bg-blue-600 text-white" : ""
-        }`}
+        className={`cursor-pointer flex flex-col items-center justify-center  w-full h-full hover:border-t-2 hover:border-t-blue-600 hover:text-blue-600 text-xl gap-1 lg:flex-row md:text-3xl
+           ${selectedPage === "gallery" ? "border-t-blue-600 border-t-2 text-blue-600" : ""
+          }`}
         onClick={() => handleClick("gallery")}
       >
-        <AiFillProduct className="text-3xl hover:text-blue-600" />
-        <span className="text-sm text-center lg:ml-2 md:text-base">גלריה</span>
+        <AiFillProduct className="text-2xl" />
+        <span className="text-xs text-center lg:ml-2 md:text-base">גלריה</span>
       </div>
 
       {/* כפץתור הוספה מרכזי */}
-      <Dropdown menu={{ items, onClick }} className="md:hidden">
+      <Dropdown menu={{ items, onClick }} className="md:hidden cursor-pointer">
         <a onClick={(e) => e.preventDefault()}>
           <PlusOutlined className="text-2xl bg-blue-600 text-white p-2 rounded-full" />
         </a>
@@ -116,16 +116,16 @@ const NavBar = () => {
       {/* הוסף בגד */}
       {/* {isLargeScreen && ( */}
       <div
-        className={`hidden md:flex flex-col items-center justify-center  w-full h-full hover:border-2 hover:border-blue-600 text-xl gap-1 lg:flex-row md:text-3xl ${
-          selectedPage === "garment_form" ? "bg-blue-600 text-white" : ""
-        }`}
+        className={`cursor-pointer hidden md:flex flex-col items-center justify-center  w-full h-full hover:border-t-2 hover:border-t-blue-600 hover:text-blue-600 text-xl gap-1 lg:flex-row md:text-3xl
+           ${selectedPage === "garment_form" ? "border-t-blue-600 border-t-2 text-blue-600" : ""
+          }`}
         onClick={() => handleClick("garment_form")}
       >
         <div className="flex flex-row items-center">
-          <FaShirt className="text-3xl hover:text-blue-600" />
+          <FaShirt className="text-2xl" />
           <BsPlus className="text-sm" />
         </div>
-        <span className="text-sm text-center lg:ml-2 md:text-base">
+        <span className="text-xs text-center lg:ml-2 md:text-base">
           יצירת בגד
         </span>
       </div>
@@ -134,16 +134,16 @@ const NavBar = () => {
       {/* צור לבוש */}
       {/* {isLargeScreen && ( */}
       <div
-        className={`hidden md:flex flex-col items-center justify-center  w-full h-full hover:border-2 hover:border-blue-600 text-xl  gap-1 lg:flex-row md:text-3xl ${
-          selectedPage === "outfit_canvas" ? "bg-blue-600 text-white" : ""
-        }`}
+        className={`cursor-pointer hidden md:flex flex-col items-center justify-center  w-full h-full hover:border-t-2 hover:border-t-blue-600 hover:text-blue-600 text-xl  gap-1 lg:flex-row md:text-3xl 
+          ${selectedPage === "outfit_canvas" ? "border-t-blue-600 border-t-2 text-blue-600" : ""
+          }`}
         onClick={() => handleClick("outfit_canvas")}
       >
         <div className="flex flex-row items-center">
-          <GiClothes className="text-4xl hover:text-blue-600" />
+          <GiClothes className="text-2xl hover:text-blue-600" />
           <BsPlus className="text-sm" />
         </div>
-        <span className="text-sm text-center lg:ml-2 md:text-base">
+        <span className="text-xs text-center lg:ml-2 md:text-base">
           יצירת לוק
         </span>
       </div>
@@ -151,22 +151,22 @@ const NavBar = () => {
 
       {/* לוח שנה   */}
       <div
-        className={`flex flex-col items-center justify-center  w-full h-full hover:border-2 hover:border-blue-600  text-xl gap-1 lg:flex-row md:text-3xl ${
-          selectedPage === "calendar" ? "bg-blue-600 text-white" : ""
-        }`}
+        className={`cursor-pointer flex flex-col items-center justify-center  w-full h-full hover:border-t-2 hover:border-t-blue-600 hover:text-blue-600  text-xl gap-1 lg:flex-row md:text-3xl 
+          ${selectedPage === "calendar" ? "border-t-blue-600 border-t-2 text-blue-600" : ""
+          }`}
         onClick={() => handleClick("calendar")}
       >
-        <IoCalendarSharp className="text-3xl hover:text-blue-600" />
-        <span className="text-sm text-center lg:ml-2 md:text-base">
+        <IoCalendarSharp className="text-3xl" />
+        <span className="text-xs text-center lg:ml-2 md:text-base">
           היסטוריה
         </span>
       </div>
 
       {/* התראות */}
       <div
-        className={`flex flex-col items-center justify-center  w-full h-full hover:border-2 hover:border-blue-600 text-xl gap-1 lg:flex-row md:text-3xl group ${
-          selectedPage === "alerts" ? "bg-blue-600 text-white" : ""
-        } `}
+        className={`cursor-pointer flex flex-col items-center justify-center  w-full h-full hover:border-t-2 hover:border-t-blue-600 hover:text-blue-600 text-xl gap-1 lg:flex-row md:text-3xl group
+           ${selectedPage === "alerts" ? "border-t-blue-600 border-t-2 text-blue-600" : ""
+          }`}
         onClick={() => handleClick("alerts")}
       >
         <Badge
@@ -174,11 +174,10 @@ const NavBar = () => {
           color="gold"
           count={AlertsCounter}
           offset={[0, 2]}
-          className="-z-1"
         >
-          <FaBell className="text-3xl hover:text-blue-600 -z-1" />
+          <FaBell className="text-2xl" />
         </Badge>
-        <span className="text-sm text-center lg:ml-2 md:text-base ">
+        <span className="text-xs text-center lg:ml-2 md:text-base ">
           התראות
         </span>
       </div>
